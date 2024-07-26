bd63a15ba8b7e6925d04e046ae71c6c3aca1e659
SebasLem2019147 <142450851+SebasLem2019147@users.noreply.github.com>
2024-07-26 14:04:02 -0600

    Update C_digo_ControlIntensidadLuz_Bluetooth.ino

--- C_digo_ControlIntensidadLuz_Bluetooth/C_digo_ControlIntensidadLuz_Bluetooth.ino
+++ C_digo_ControlIntensidadLuz_Bluetooth/C_digo_ControlIntensidadLuz_Bluetooth.ino
@@ -1,3 +1,15 @@
+/*
+   Fundacion Kinal
+   Centro educativo tecnico laboral Kinal
+   Quinto perito
+   Quinto electronica
+   Codigo Tecnico: EB5AV 
+   Curso: Taller de electronica digital y reparacion de computadoras I
+   Dev: Sebastian Enrique Lemus Salvador
+   Fecha: 26 de julio de 2024
+     
+*/
+
 #include <SoftwareSerial.h>
 
 int Tx = 5;
